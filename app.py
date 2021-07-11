05843c1b7194e96124a476543e64250e7ae98d5b
EniasVontas <48837905+EniasVontas@users.noreply.github.com>
2021-07-11 18:13:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = Flask(__name__) #Initialize the flask App
 def home():
     return render_template("index.html")
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict',methods=['GET'])
 def predict():
     model = pickle.load(open('model.pkl', 'rb'))
     age = request.args.get('age')
